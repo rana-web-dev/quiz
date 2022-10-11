de7c9d85b2f98bc2f8873525bf731baafe62064c
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-10-11 10:25:44 +0000

    Hello

--- src/components/quizContent/QuizContent.js
+++ src/components/quizContent/QuizContent.js
@@ -1,48 +1,54 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 const QuizContent = () => {
-    const quizContent = useLoaderData();
-    const question = quizContent.data.questions;
-    return (
-        <div>
-            {
-                question.map(q => <Question
-                    key={q.id}
-                    questions={q}
-                ></Question>)
-            }
-        </div>
-    );
-};
+  const quizContent = useLoaderData()
+  const question = quizContent.data.questions
 
-const Question = ({questions}) => {
-    const {options, question, correctAnswer} = questions;
-
-    console.log(questions)
-    return (
-        <div className='max-w-6xl mx-auto'>
-            <p className='font-bold text-2xl mt-5'>{question}</p>
-            <div className='grid grid-cols-2 cursor-default w-1/2'>
-            {
-                options.map(op => <Options 
-                    option={op}
-                    correctAnswer={correctAnswer}
-                ></Options>)
-            }
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      {question.map(q => (
+        <Question key={q.id} questions={q} />
+      ))}
+    </div>
+  )
 }
 
-const Options = ({option, correctAnswer}) => {
+const Question = ({ questions }) => {
+  const { options, question, correctAnswer } = questions
+  const handleOptionClick = ans => {
+    const isCorrect = answer === correctAnswer
 
-    return (
-        <div>
-            <p  className='bg-blue-500 m-2 text-white p-2 rounded'>{option}</p>
-        </div>
-    )
+    alert(`Correct: ${isCorrect}`)
+  }
+
+  return (
+    <div className="max-w-6xl mx-auto">
+      <p className="font-bold text-2xl mt-5">{question}</p>
+      <div className="grid grid-cols-2 cursor-default w-1/2">
+        {options.map((op, ind) => (
+          <Options key={ind} option={op} onOptionClick={handleOptionClick} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
+const Options = ({ option, onOptionClick }) => {
+  const handleClick = () => {
+    onOptionClick(option)
+  }
 
-export default QuizContent;
+  return (
+    <div>
+      <p
+        onClick={handleClick}
+        className="bg-blue-500 m-2 text-white p-2 rounded"
+      >
+        {option}
+      </p>
+    </div>
+  )
+}
+
+export default QuizContent
